411179d9117fbe9185d362a908b588b0a082b7e6
Adam <adamvictorventura@gmail.com>
2022-01-02 11:18:48 -0600

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -1,15 +1,14 @@
 from words import words
 import random
 
-#picks a random word and makes a list of letters
 word_item = random.randint(0, 1521)
 word = words[word_item]
 word_length = len(word)
-print(word_length)
 letters = list(word)
 print(word)
 print(letters)
 print(f"there are {word_length} letters in the word")
+
 
 letters_added = 0
 display_word_list = []
@@ -26,22 +25,24 @@ while letters_added < word_length:
 
 word_output = display_word(display_word_list)
 print(word_output)
+
 def add_letters(letter_order, guess):
-    letter_index = letter_order -1
+    
+    letter_index = letter_order-1
     letters_added = 0
     while letters_added <= word_length:
         if letters_added == letter_index:
            display_word_list[letter_index] = guess
            break
         else:
-            letters_added += 1
-    
+            letters_added+=1
     return display_word_list
-    
+
 correct_guesses = 0
 guesses = 0
 wrong_guesses = 0
-while wrong_guesses < 4:
+
+while wrong_guesses < 6:
     guess = input("enter a letter: ")
     for letter in letters: 
         if guess == letter:
@@ -52,4 +53,4 @@ while wrong_guesses < 4:
             word_output = display_word(display_word_list)
             print(word_output)
     guesses += 1
-    wrong_gueses = guesses - correct_guesses
+    wrong_guesses = guesses - correct_guesses
